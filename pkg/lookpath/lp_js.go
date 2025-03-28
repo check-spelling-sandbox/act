e395308161fd404e6bb01a51bb5830ada6ade5fd
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-03-28 13:50:43 -0400

    spelling: cannot
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/lookpath/lp_js.go
+++ pkg/lookpath/lp_js.go
@@ -18,6 +18,6 @@ var ErrNotFound = errors.New("executable file not found in $PATH")
 // If file contains a slash, it is tried directly and the PATH is not consulted.
 // The result may be an absolute path or a path relative to the current directory.
 func LookPath2(file string, lenv Env) (string, error) {
-	// Wasm can not execute processes, so act as if there are no executables at all.
+	// Wasm cannot execute processes, so act as if there are no executables at all.
 	return "", &Error{file, ErrNotFound}
 }
