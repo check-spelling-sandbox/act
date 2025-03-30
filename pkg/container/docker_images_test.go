c682b616a9ead9495e30145b0361675bea9d9fd3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-03-30 13:41:06 -0400

    spelling: one
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/container/docker_images_test.go
+++ pkg/container/docker_images_test.go
@@ -21,7 +21,7 @@ func TestImageExistsLocally(t *testing.T) {
 	}
 	ctx := context.Background()
 	// to help make this test reliable and not flaky, we need to have
-	// an image that will exist, and onew that won't exist
+	// an image that will exist, and one that won't exist
 
 	// Test if image exists with specific tag
 	invalidImageTag, err := ImageExistsLocally(ctx, "library/alpine:this-random-tag-will-never-exist", "linux/amd64")
